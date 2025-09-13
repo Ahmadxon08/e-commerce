b35858c7aa6286371ee690030db809dc672973fa
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2025-09-14 00:17:33 +0500

    admin structure was added

---
+++ app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Nav, { NavLink } from "@/components/Nav";
+type AdminLayoutProps = {
+  children: React.ReactNode;
+};
+
+const AdminLayout = ({ children }: Readonly<AdminLayoutProps>) => {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/product">Products</NavLink>
+        <NavLink href="/admin/users">Costumers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+
+      <div className="container my-6">{children}</div>
+    </>
+  );
+};
+
+export default AdminLayout;
---
+++ components/Nav.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { ReactNode } from "react";
+import { ComponentProps } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const Nav = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="bg-primary text-primary-foreground  flex  justify-center px-3">
+      {children}
+    </div>
+  );
+};
+
+export default Nav;
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+  const pathname = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        "p-4 hover:bg-secondary hover:text-secondary-foreground  focus-visible:bg-secondary focus-visible:text-secondary-foreground ",
+        pathname === props.href && "bg-background text-foreground"
+      )}
+    />
+  );
+}
