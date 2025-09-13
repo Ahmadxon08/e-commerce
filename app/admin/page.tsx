2a56da967700cd6f8c91cb45251753aa94365130
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2025-09-14 00:42:56 +0500

    updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
+function getSalesData() {}
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="grid  px-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Total Revenue"
+        desc="Total revenue generated"
+        price="2222$"
+      />
+    </div>
+  );
 };
 
+type DashboardCardProps = {
+  title: string;
+  desc: string;
+  price: string;
+};
+
+export function DashboardCard({ title, desc, price }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">{title}</CardTitle>
+        <CardDescription>{desc}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className="text-3xl font-bold">{price}</p>
+      </CardContent>
+    </Card>
+  );
+}
 export default AdminPage;
