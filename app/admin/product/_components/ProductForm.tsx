22963e541a849666d92ecc738b9d82dbcbaa4134
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2025-09-14 21:53:27 +0500

    updated

--- app/admin/product/_components/ProductForm.tsx
+++ app/admin/product/_components/ProductForm.tsx
@@ -1,6 +1,19 @@
 "use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const ProductForm = () => {
-  return <div>ProductFrom</div>;
+  return (
+    <div>
+      <form className="space-y-8">
+        <div className=" space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" placeholder="Product Name" id="name" required />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ProductForm;
